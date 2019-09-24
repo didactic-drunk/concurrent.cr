36b19dd1fb187d695a94372612974ac2a27bafcb
Simon Cruanes <simon.cruanes.2007@m4x.org>
2019-09-24 17:10:09 -0500

    update to the semaphore impl

--- spec/concurrent/semaphore_spec.cr
+++ spec/concurrent/semaphore_spec.cr
@@ -13,19 +13,27 @@ describe Semaphore do
   it "allows at most n concurrent accesses" do
     n = 20
     n_active = Atomic.new 0
+    n_sum = Atomic.new 0
     sem = Semaphore.new n
+    close = Channel(Nil).new
 
     # spawn a lot of fibers
-    3000.times do
+    3000.times do |i|
       spawn do
+        Fiber.yield
         sem.acquire do
           x = n_active.add 1
           x.should(be <= n)
-          Fiber.yield
         ensure
           n_active.sub 1
+          n_sum.add 1
         end
+        close.send(nil)
       end
     end
+
+    3000.times { close.receive }
+
+    n_sum.get.should eq(3000)
   end
 end
--- src/concurrent/semaphore.cr
+++ src/concurrent/semaphore.cr
@@ -1,4 +1,5 @@
 
+# TODO: use crystal's spin lock
 #require "crystal/spin_lock"
 class SpinLock
   def sync(&block)
@@ -12,39 +13,39 @@ class Concurrent::Semaphore
   # Will raise if `n <= 0`.
   def initialize(n : Int32)
     raise ArgumentError.new "semaphore needs positive argument, not #{n}" unless n > 0
-    @count = n
+    @count = Atomic(Int32).new n
     @lock = SpinLock.new
-    @waiters = Deque(Fiber).new
+    @waiters = Deque(Fiber).new 10
   end
 
   # Acquire an item from the semaphore, calling the block, and then safely
   # releasing the semaphore.
   def acquire(&block)
     # try to acquire now
-    acquired =
-      @lock.sync do
-        if (n = @count) > 0
-          @count -= 1
-          true
-        else
-          false
-        end
+    loop do
+      n_cur = @count.get
+
+      if n_cur == 0
+        @lock.sync { @waiters << Fiber.current }
+        Fiber.yield
+        break
       end
-    if acquired
+
+      n_old, swapped = @count.compare_and_set(n_cur, n_cur-1)
+      next if !swapped # race condition
+
       begin
         yield
       ensure
-        @lock.sync do
-          @count += 1
-          # try to wake up a waiter
-          if fiber = @waiters.shift?
+        @count.add(1)
+        # try to wake up a waiter, if any
+        if @waiters.size > 1
+          if fiber = @lock.sync { @waiters.shift? }
             fiber.resume
           end
         end
       end
-    else
-      @lock.sync { @waiters << Fiber.current }
-      Fiber.yield
+      break
     end
-  end
+    end
 end
