49b5c2b6d8d2d0432526cfa584b78b69b17d7555
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-01-10 17:37:31 -0600

    modify forkjoin handle oscillating counters

--- src/concurrent/fork_join.cr
+++ src/concurrent/fork_join.cr
@@ -1,17 +1,23 @@
 require "atomic"
+require "./count_down_latch"
 
 # A fork-join pool provides an easy way to spawn a bunch of fibers,
 # and then wait for all of them to complete.
 class Concurrent::ForkJoin
   protected def initialize
-    @close = Channel(Nil).new
+    @wait = Channel(Nil).new 1
+    @all_started = false  # becomes true after the block calling `fork` is done
     @n = Atomic(Int32).new 0
+  end
+
+  protected def all_started
+    @all_started = true
   end
 
   # wait for all tasks to be done
   protected def join
-    return if @n.get == 0
-    @close.receive
+    return if @all_started && @n.get == 0
+    @wait.receive
   end
 
   # Start a fork-join pool, and call the block with it.
@@ -19,6 +25,7 @@ class Concurrent::ForkJoin
     f = ForkJoin.new
     begin
       yield f
+      f.all_started
       Fiber.yield
     ensure
       f.join
@@ -26,15 +33,16 @@ class Concurrent::ForkJoin
   end
 
   def fork(&block)
+    raise Exception.new "fork join: already done" if @all_started
     @n.add 1
     # start a new fiber
     spawn do
       block.call
     ensure
-      x = @n.sub 1
-      raise Exception.new "what" if x < 1
-      if x == 1
-        @close.send nil
+      prev_n = @n.sub 1
+      raise Exception.new "fork join: internal error" if prev_n < 1
+      if @all_started && prev_n == 1
+        @wait.send nil
       end
     end
   end
