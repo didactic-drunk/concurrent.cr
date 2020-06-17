59562d15c61f9eed2a8e8c32a78a9ddaebea286a
Didactic Drunk <1479616+didactic-drunk@users.noreply.github.com>
2020-06-16 18:06:21 -0700

    Typos

--- src/concurrent/stream.cr
+++ src/concurrent/stream.cr
@@ -3,12 +3,13 @@ require "./wait"
 # Influenced by [Ruby parallel](https://github.com/grosser/parallel)
 # and [Java streams](https://docs.oracle.com/javase/8/docs/api/java/util/stream/Stream.html).
 #
-# Channel#parallel creates a Stream::Source reading from the Channel.
-# Enumerable#parallel creates a Channel and Stream::Source reading from it.
+# ## Creating a stream:
+# * Channel#parallel creates a Stream::Source reading from the Channel.
+# * Enumerable#parallel creates a Channel and Stream::Source reading from it.
 #
 # ## Stream operations:
-# * #map { } - Same as Enumereable#map but runs in a fiber pool.
-# * #select { } - Same as Enumereable#select but runs in a fiber pool.
+# * #map { } - Same as Enumerable#map but runs in a fiber pool.
+# * #select { } - Same as Enumerable#select but runs in a fiber pool.
 # * #run { } - Runs block in a fiber pool.  Further processing is not possible except for #wait.
 # * #tee { } - Runs block in a fiber pool passing the original message to the next Stream.
 # * #serial - returns an Enumerable collecting results from a parallel Stream.
